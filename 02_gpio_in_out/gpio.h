235592ff2e51d4e13c2729bf6af6b06c2b3bd84b
hendrixjl <hendrix.john64@gmail.com>
2016-08-04 13:41:03 -0500

    using gpio as input

--- 02_gpio_in_out/gpio.h
+++ 02_gpio_in_out/gpio.h
@@ -8,11 +8,31 @@ volatile unsigned int* const GPFSEL2 = (unsigned int*)0x20200008;
 volatile unsigned int* const GPFSEL3 = (unsigned int*)0x2020000C;
 volatile unsigned int* const GPFSEL4 = (unsigned int*)0x20200010;
 volatile unsigned int* const GPFSEL5 = (unsigned int*)0x20200014;
+volatile unsigned int* const GPSET0 =  (unsigned int*)0x2020001C;
+volatile unsigned int* const GPSET1 =  (unsigned int*)0x20200020;
+volatile unsigned int* const GPCLR0 =  (unsigned int*)0x20200028;
+volatile unsigned int* const GPCLR1 =  (unsigned int*)0x2020002C;
+const volatile unsigned int* GPLEV0 =  (unsigned int*)0x20200034;
+const volatile unsigned int* GPLEV1 =  (unsigned int*)0x20200038;
+volatile unsigned int* GPEDS0 =        (unsigned int*)0x20200040;
+volatile unsigned int* GPEDS1 =        (unsigned int*)0x20200044;
+volatile unsigned int* GPREN0 =        (unsigned int*)0x2020004C;
+volatile unsigned int* GPREN1 =        (unsigned int*)0x20200050;
+volatile unsigned int* GPFEN0 =        (unsigned int*)0x20200058;
+volatile unsigned int* GPFEN1 =        (unsigned int*)0x2020005C;
+volatile unsigned int* GPHEN0 =        (unsigned int*)0x20200064;
+volatile unsigned int* GPHEN1 =        (unsigned int*)0x20200068;
+volatile unsigned int* GPLEN0 =        (unsigned int*)0x20200070;
+volatile unsigned int* GPLEN1 =        (unsigned int*)0x20200074;
+volatile unsigned int* GPAREN0 =       (unsigned int*)0x2020007C;
+volatile unsigned int* GPAREN1 =       (unsigned int*)0x20200080;
+volatile unsigned int* GPAFEN0 =       (unsigned int*)0x20200088;
+volatile unsigned int* GPAFEN1 =       (unsigned int*)0x2020008C;
+volatile unsigned int* GPPUD =         (unsigned int*)0x20200094;
+volatile unsigned int* GPPUDCLK0 =     (unsigned int*)0x20200098;
+volatile unsigned int* GPPUDCLK1 =     (unsigned int*)0x2020009C;
 
-volatile unsigned int* const GPSET0 = (unsigned int*)0x2020001C;
-volatile unsigned int* const GPSET1 = (unsigned int*)0x20200020;
-volatile unsigned int* const GPCLR0 = (unsigned int*)0x20200028;
-volatile unsigned int* const GPCLR1 = (unsigned int*)0x2020002C;
+
 
 enum gpio_function {
     GPIO_FN_INPUT = 0,
@@ -33,6 +53,69 @@ enum gpio {
     gpio32, gpio33, gpio34, gpio35, gpio36, gpio37, gpio38, gpio39,
     gpio40, gpio41, gpio42, gpio43, gpio44, gpio45, gpio46, gpio47,
     gpio48, gpio49, gpio50, gpio51, gpio52, gpio53
+};
+
+enum gpio_bitmasks {
+    gpio00_bit = 0x00000001, 
+    gpio01_bit = 0x00000002, 
+    gpio02_bit = 0x00000004, 
+    gpio03_bit = 0x00000008, 
+    gpio04_bit = 0x00000010, 
+    gpio05_bit = 0x00000020, 
+    gpio06_bit = 0x00000040, 
+    gpio07_bit = 0x00000080,
+    gpio08_bit = 0x00000100, 
+    gpio09_bit = 0x00000200, 
+    gpio10_bit = 0x00000400, 
+    gpio11_bit = 0x00000800, 
+    gpio12_bit = 0x00001000, 
+    gpio13_bit = 0x00002000, 
+    gpio14_bit = 0x00004000, 
+    gpio15_bit = 0x00008000,
+    gpio16_bit = 0x00010000, 
+    gpio17_bit = 0x00020000, 
+    gpio18_bit = 0x00040000, 
+    gpio19_bit = 0x00080000, 
+    gpio20_bit = 0x00100000, 
+    gpio21_bit = 0x00200000, 
+    gpio22_bit = 0x00400000, 
+    gpio23_bit = 0x00800000,
+    gpio24_bit = 0x01000000, 
+    gpio25_bit = 0x02000000, 
+    gpio26_bit = 0x03000000, 
+    gpio27_bit = 0x08000000, 
+    gpio28_bit = 0x10000000, 
+    gpio29_bit = 0x20000000, 
+    gpio30_bit = 0x40000000, 
+    gpio31_bit = 0x80000000,
+    gpio32_bit = 0x00000001, 
+    gpio33_bit = 0x00000002, 
+    gpio34_bit = 0x00000004, 
+    gpio35_bit = 0x00000008, 
+    gpio36_bit = 0x00000010, 
+    gpio37_bit = 0x00000020, 
+    gpio38_bit = 0x00000040, 
+    gpio39_bit = 0x00000080,
+    gpio40_bit = 0x00000100, 
+    gpio41_bit = 0x00000200, 
+    gpio42_bit = 0x00000400, 
+    gpio43_bit = 0x00000800, 
+    gpio44_bit = 0x00001000, 
+    gpio45_bit = 0x00002000, 
+    gpio46_bit = 0x00004000, 
+    gpio47_bit = 0x00008000,
+    gpio48_bit = 0x00010000, 
+    gpio49_bit = 0x00020000, 
+    gpio50_bit = 0x00040000, 
+    gpio51_bit = 0x00080000, 
+    gpio52_bit = 0x00100000, 
+    gpio53_bit = 0x00200000
+};
+
+enum GPIO_PUD_state {
+    PUD_off = 0,
+    PUD_enable_pull_down = 1,
+    PUD_enable_pull_up = 2
 };
 
 inline void gpio_fsel(enum gpio pin, enum gpio_function fun)
@@ -89,3 +172,27 @@ inline void gpio_clear(enum gpio pin)
         *GPCLR1 = set_bit_pattern(*GPCLR1, CLEAR, pin-32, ONE_BIT);
     }
 }
+
+inline unsigned int gpio_level(enum gpio pin)
+{
+    enum { ONE_BIT=1 };
+    return ((pin >= gpio00) && (pin <= gpio31)) ?
+            get_bit_pattern(*GPLEV0, pin, ONE_BIT) :
+            get_bit_pattern(*GPLEV1, pin-32, ONE_BIT);
+}
+
+inline void set_pud(enum gpio pin, enum GPIO_PUD_state pud)
+{
+    *GPPUD = pud;
+    for (volatile int i=0; i<150; ++i);
+    if ((pin >= gpio00) && (pin <= gpio31))
+    {
+        set_bit_pattern(*GPPUDCLK0, 1, pin, 1);
+    }
+    else
+    {
+        set_bit_pattern(*GPPUDCLK1, 1, pin-32, 1);
+    }
+    for (volatile int i=0; i<150; ++i);
+}
+
