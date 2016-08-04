015d579602151397440d08a80e7653f326a1ac94
hendrixjl <hendrix.john64@gmail.com>
2016-08-04 13:40:57 -0500

    using gpio as input

--- 02_gpio_in_out/demo.c
+++ 02_gpio_in_out/demo.c
@@ -1,18 +1,30 @@
 #include "utils.h"
-#include "mmregisters.h"
+#include "gpio.h"
 
 void startc ( void )
 {
     gpio_fsel(gpio47, GPIO_FN_OUTPUT);
     gpio_fsel(gpio23, GPIO_FN_OUTPUT);
+    gpio_fsel(gpio24, GPIO_FN_INPUT);
 
     while(1)
     {
         gpio_set(gpio47);
-        gpio_clear(gpio23);
-        for (volatile int busy=0; busy<0x200000; busy++);
+
+        enum { HIGH = 1 };
+        if (gpio_level(gpio24) == HIGH)
+        {
+            gpio_set(gpio23);
+        }
+        else
+        {
+            gpio_clear(gpio23);
+        }
+
+        for (volatile int busy=0; busy<0x400000; busy++);
+
         gpio_clear(gpio47);
-        gpio_set(gpio23);
-        for (volatile int busy=0; busy<0x200000; busy++);
+
+        for (volatile int busy=0; busy<0x400000; busy++);
     }
 }
